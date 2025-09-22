f951b7d7761d96719039de48bbb9460310ceb85b
Aniket <aniket.97.mane@gmail.com>
2025-09-22 21:49:52 +0530

    Generating Fibonacci sequence as placeholder code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Fibonacci Sequence");
+
+    let n = 10;
+    let mut sequence = vec![0, 1];
+
+    for i in 2..n {
+        let next = sequence[i - 1] + sequence[i - 2];
+        sequence.push(next);
+    }
+
+    println!("First {} Fibonacci numbers: {:?}", n, sequence);
 }
